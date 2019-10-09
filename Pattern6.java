222a6c995a73e5556531b1652201c90d67e3a596
Tushar1401 <54734803+Tushar1401@users.noreply.github.com>
2019-10-09 12:42:27 +0530

    PATTERN IN JAVA

---
+++ Pattern6.java
@@ -0,0 +1,33 @@
+package PATTERNS;
+
+import java.util.Scanner;
+
+public class Pattern6 
+{
+
+	public static void main(String[] args) 
+	{
+			
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("ENTER A NUMBER - ");
+	    int n = sc.nextInt();
+        int no=1;	    
+	    for(int i=1;i<=n;i++)
+	     {
+	    	 for(int j=1;j<=n-i;j++)
+	    	 {
+	    		  System.out.print("   ");
+	    		  
+	    	 }
+	    	 for(int j=1;j<=i;j++)
+	    	 {
+	    		 System.out.print(no++ + "    ");
+	    	 }
+	      System.out.println();
+	     }
+	    
+		
+
+	}
+
+}
